b13ba6719c061412a30ae15b859959fb818214d0
Spoonzio <jpan569@gmail.com>
2019-10-12 21:49:05 +0200

    created fucntions in class

--- qt5/main.py
+++ qt5/main.py
@@ -1,13 +1,16 @@
 import random
 import time
 import sys, os
+import numpy as np
 
 from PyQt5 import QtCore, QtGui
 from PyQt5.uic import loadUi
 from PyQt5.QtWidgets import *
 
 class SudokuGame(QMainWindow):
-    
+    # Class var
+    number_set = [[0 for i in range(9)] for j in range(9)]
+
     def __init__(self):
         # Constructor
         QMainWindow.__init__(self)
@@ -18,10 +21,29 @@ class SudokuGame(QMainWindow):
 
         self.setWindowIcon(QtGui.QIcon("sudoku.png"))
         
-        # Connect methods to buttons :
-        # self.btnGenerate.clicked.connect(self.generate)
+        # Connect methods to buttons
+        self.btnGenerate.clicked.connect(self.generate)
         # self.btnReset.clicked.connect(self.reset)
         # self.btnSolve.clicked.connect(self.solve)
+
+    def generate(self):
+        # Will create solved board first
+        pass
+
+
+    def solve(self):
+        # Populate 2d array with numbers
+        pass
+
+
+    def show_board(self):
+        # Display 2d array into GUI
+        pass
+
+
+    def reset(self):
+        # Reset class 2d array
+        self.number_set = [[0 for i in range(9)] for j in range(9)]
 
 
 app = QApplication([])
